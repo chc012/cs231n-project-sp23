7bc620512bff04627b7da971ce575a2f89aa9e86
Cheng Chang <chc012@stanford.edu>
2023-05-14 02:22:55 -0700

    milestone scripts and experiments

--- RESNET/APM/apm_util.py
+++ RESNET/APM/apm_util.py
@@ -1,13 +1,6 @@
-# import timm
-# import torch
-# from PIL import Image
-# from timm.data import resolve_data_config
-# import torchvision.transforms as transforms
-# from timm.data.transforms_factory import create_transform
-# import matplotlib.pyplot as plt
-
 from torchvision.transforms import Resize, CenterCrop, ToPILImage
 from torchvision.transforms import Normalize
+from torch import topk
 
 
 def inverse_transform(image):
@@ -22,3 +15,25 @@ def inverse_transform(image):
     image = CenterCrop(size=(235, 235))(image)
     image = Resize(size=(224, 224), interpolation=2)(image)
     return image
+
+def print_topk(categories, probabilities, k):
+    """Print top categories per image"""
+    topk_prob, topk_catid = topk(probabilities, k)
+    for i in range(topk_prob.size(0)):
+        print(categories[topk_catid[i]], topk_prob[i].item())
+
+def compare_probs(categories, prob_before, prob_after, verbose=False):
+    """
+    Print probability of the classification of original image
+    before and after spoofing.
+    """
+    prob_before, id = topk(prob_before, 1)
+    prob_before, id = prob_before[0], id[0]
+    prob_after = prob_after[id].detach()
+
+    if verbose:
+        print("Original category: {}".format(categories[id]))
+        print("Probability before: {}".format(prob_before))
+        print("Probability after: {}".format(prob_after))
+    
+    return categories[id], prob_before, prob_after
